63d131daf6de06da823c572c033416c926bb1d6c
KhyatiSatija <satijakhyati2003@gmail.com>
2025-04-20 17:26:43 +0530

    understood the directory structure and set up Angular Material for the Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,6 @@
+// app.component.ts is the source file that describes the app-root component. This is the top-level Angular component in the app.
+//  A component is the basic building block of an Angular application. The component description includes the component's code,
+//  HTML template, and styles, which can be described in this file, or in separate files.
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
